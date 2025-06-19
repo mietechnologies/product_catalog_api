fb42eb7ec77093db51b3f41c10974bae4aa716a8
Brett Chapin <brett.chapin89@gmail.com>
2025-06-18 21:11:42 -0400

    Updated server.js to use the docker set environment value for the mongodb URI

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config({ path: './configs/config.env' });
-
-const isDevelopment = process.env.NODE_ENV === 'development';
-const DB = (isDevelopment ? process.env.DEV_DATABASE : process.env.DATABASE).replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.MONGO_URI;
 
 mongoose.connect(DB).then(() => console.log('DB connected successfully!'));
 
